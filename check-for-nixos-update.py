1134a5ff35563d6785261e9ad5324328f669e007
Michael Jansen <mjansen@tpg.com.au>
2018-08-28 23:58:52 +1000

    avoid use of local file system as storage medium by using S3 directly

--- check-for-nixos-update.py
+++ check-for-nixos-update.py
@@ -9,30 +9,15 @@ import certifi
 import boto3
 import botocore
 
-configFile  = '/tmp/config.yaml'
-lastRelease = '/tmp/lastRelease'
-
 def nixosChannelURL(nixosVersion):
     return ('https://nixos.org/channels/nixos-' + nixosVersion + '/')
 
-def loadConfig():
-    with open(configFile, 'r') as stream:
-        try:
-            return (yaml.load(stream))
-        except yaml.YAMLError as exc:
-            print(exc)
-            raise(exc)
-
-def readContent(fileName):
+def loadConfig(str):
     try:
-        with open(fileName, 'r') as stream:
-            return stream.read()
-    except:
-        return ''
-
-def writeContent(fileName, myData):
-    with open(fileName, 'w') as stream:
-        stream.write(myData)
+        return (yaml.load(str))
+    except yaml.YAMLError as exc:
+        print(exc)
+        raise(exc)
 
 def sendSlackMessage(connPool, slackURL, msg):
     data = { 'text': msg }
@@ -44,15 +29,14 @@ def sendSlackMessage(connPool, slackURL, msg):
         print('cannot send to slack: status = {}, data = {}'.format(result.status, result.data))
 
 BUCKET_NAME = os.environ['BUCKET_NAME']
-KEY = 'config.yaml'
 
 def my_handler(event, context):
     print(event)
-    print(context)
+    # print(context)
     s3 = boto3.resource('s3')
 
     try:
-        s3.Object(BUCKET_NAME, 'config.yaml').download_file('/tmp/config.yaml')
+        cfgStr = s3.Object(BUCKET_NAME, 'config.yaml').get()['Body'].read().decode('utf-8')
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             print("cannot find the configuration file 'config.yaml'.")
@@ -60,37 +44,31 @@ def my_handler(event, context):
             raise
 
     try:
-        s3.Object(BUCKET_NAME, 'lastRelease').download_file(lastRelease)
+        oldLocation = s3.Object(BUCKET_NAME, 'lastRelease').get()['Body'].read().decode('utf-8')
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             print("cannot find the object 'lastRelease'.  This will be created.")
+            oldLocation = ''
 
     # fetch our parameters from the configuration file:
 
-    cfg = loadConfig()
-    os.remove('/tmp/config.yaml')
+    cfg = loadConfig(cfgStr)
     nixosVersion = cfg['nixosVersion']
     slackURL     = cfg['slackURL']
-
-    # we read the last release URL to later compare it with the current one:
-
-    oldLocation = readContent(lastRelease)
 
     # query nixos.org to examine what the latest release of this nixos version is:
 
     connPool    = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
     result      = connPool.request('HEAD', nixosChannelURL(nixosVersion), redirect=False)
     newLocation = result.headers['Location']
-    writeContent(lastRelease, newLocation)
 
-    try:
-        s3.Object(BUCKET_NAME, 'lastRelease').upload_file(lastRelease)
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            print("cannot upload the object 'lastRelease'.")
-        raise
-
-    os.remove(lastRelease)
+    if newLocation != oldLocation:
+        try:
+            s3.Object(BUCKET_NAME, 'lastRelease').put(Body = newLocation.encode('utf-8'))
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                print("cannot upload the object 'lastRelease'.")
+                raise
 
     # now inform the slack channel of the current release of that nixos version:
 
