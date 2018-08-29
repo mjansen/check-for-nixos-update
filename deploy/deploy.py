4db6bcdfc56e29ec8e24ba95bed881edefbbfb2c
Michael Jansen <mjansen@tpg.com.au>
2018-08-30 05:02:29 +1000

    make this a little more generic

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -6,12 +6,7 @@ import botocore
 import boto3
 import time
 
-topDir = '..'
-
-cfTemplateFile = topDir + '/' + 'resources.yaml'
-pySourceFile   = topDir + '/' + 'check_for_nixos_update.py'
-
-lastDeployedVersionFile = 'lastDeployedVersion'
+stackName      = 'check-for-nixos-update'
 
 def compute_version():
     """Compute the sha1 hash of the sources as the current version."""
@@ -22,32 +17,38 @@ def compute_version():
         m.update(stream.read())
     return m.hexdigest()
 
-def readLastVersion():
-    """Read the last published version from disk, returned as a string.."""
-    # we should read from S3 or cloudformation
-    try:
-        with open(lastDeployedVersionFile, 'rb') as stream:
-            lastVersion = stream.read().decode('utf-8')
-    except:
-        lastVersion = ''
-
-    return lastVersion
-
-def saveVersion(version):
-    """Save the currently deployed version to the file system."""
-    # we should write to S3 or cloudformation
-    with open(lastDeployedVersionFile, 'wb') as stream:
-        stream.write(version.encode('utf-8'))
+def findOutput(tag, outputs):
+    """Find and output variable for the stack"""
+    for o in outputs:
+        if o['OutputKey'] == tag:
+            return o['OutputValue']
+    raise ValueError
 
 if __name__ == '__main__':
 
+    # initially fetch the stack state:
+
+    cf = boto3.client('cloudformation')
+
+    before = cf.describe_stacks(StackName = stackName)
+    print(before)
+    if 'Stacks' not in before.keys() or len(before['Stacks']) != 1:
+        print('we are not getting a good result describing the stack')
+        sys.exit(1)
+    stateBefore = before['Stacks'][0]
+
+    sourceName     = findOutput('StackLambdaCode', stateBefore['Outputs'])
+    templName      = 'resources.yaml'
+
+    topDir = '..'
+
+    cfTemplateFile = topDir + '/' + templName
+    pySourceFile   = topDir + '/' + sourceName + '.py'
+
     # have the source files changed?
 
-    lastVersion = readLastVersion()
+    lastVersion = findOutput('StackVersion', stateBefore['Outputs'])
     currVersion = compute_version()
-
-    print(lastVersion)
-    print(currVersion)
 
     if lastVersion == currVersion:
         print('no change')
@@ -63,29 +64,30 @@ if __name__ == '__main__':
 
     # the python code is also ok, so let us invoke cloudformation:
 
-    os.system('cat ../resources.yaml | sed -e "s/VERSION/{}/" > resources.yaml'.format(currVersion))
     # how is tmp built?
-    os.system('cp ' + pySourceFile + ' tmp/check_for_nixos_update.py')
+    os.system('cp ' + pySourceFile + ' tmp/')
     print('zipping lambda deployment')
     os.system('cd tmp && zip -qr ../{}.zip *'.format(currVersion))
     print('uploading to s3')
 
+    zipFile   = '{}.zip'.format(currVersion)
+
+    # the bucket does not exist until we have created the stack, or at
+    # least a part of it that creates the bucket.  Perhaps all stacks
+    # should have a stack-bucket they can refer to.
+
+    bucket = findOutput('StackBucket', stateBefore['Outputs'])
+
     s3 = boto3.resource('s3')
-    bucket = 'check-for-nixos-update-mybucket-1odydo854iyre'
-    zipFile = '{}.zip'.format(currVersion)
+
     s3.Object(bucket, zipFile).upload_file(zipFile)
     print('done uploading')
 
-    cf = boto3.client('cloudformation')
-
-    before = cf.describe_stacks(StackName = 'check-for-nixos-update')
-    print(before)
-
-    with open('resources.yaml', 'rb') as stream:
-        t = stream.read().decode('utf-8')
+    with open(cfTemplateFile, 'rb') as stream:
+        t = stream.read().decode('utf-8').replace('__VERSION__', currVersion)
 
     try:
-        result = cf.update_stack(StackName = 'check-for-nixos-update', TemplateBody = t, Capabilities = [ 'CAPABILITY_IAM' ])
+        result = cf.update_stack(StackName = stackName, TemplateBody = t, Capabilities = [ 'CAPABILITY_IAM' ])
     except botocore.exceptions.ClientError as e:
         print('===========')
         print(e)
@@ -98,14 +100,13 @@ if __name__ == '__main__':
     print(result)
 
     while True:
-        after = cf.describe_stacks(StackName = 'check-for-nixos-update')
+        after = cf.describe_stacks(StackName = stackName)
         print(after)
         status = after['Stacks'][0]['StackStatus']
         print('status =', status)
         if after['Stacks'][0]['LastUpdatedTime'] != before['Stacks'][0]['LastUpdatedTime']:
             if status.find('COMPLETE') >= 0:
                 break
-        time.sleep(10)
+        time.sleep(15)
 
-    saveVersion(currVersion)
     sys.exit(0)
